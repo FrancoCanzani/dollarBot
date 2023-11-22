2846bcdfa2b3f0a1a5a3d66024099d4f971cacc8
Franco Canzani <francocanzani@gmail.com>
2023-11-22 22:39:20 +0100

    Adds stock scrapping

--- src/main.py
+++ src/main.py
@@ -1,63 +1,50 @@
-import requests
 from bs4 import BeautifulSoup
-from twitter import Twitter  # Ensure this is correctly imported
+from twitter import Twitter
+from scrapper import Scrapper
+from datetime import datetime
+import pytz
 
-# Usage
 twitter = Twitter()
 
-# Set a user agent to avoid a server error while trying to scrape
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
 }
-url = "https://www.cronista.com/informacion-de-mercados/"
-r = requests.get(url, headers=headers)
+url_infobae = "https://www.infobae.com/economia/divisas/dolar-hoy/"
+url_invertir = "https://iol.invertironline.com/mercado/cotizaciones/argentina"
 
-soup = BeautifulSoup(r.text, "html.parser")
+infobae_scrapper = Scrapper(url_infobae, headers)
+invertir_online_scrapper = Scrapper(url_invertir, headers)
+
+data = infobae_scrapper.get_data()
+data2 = invertir_online_scrapper.get_data()
+
+soup = BeautifulSoup(data, "html.parser")
+soup2 = BeautifulSoup(data2, "html.parser")
 
 prices = []
 
-for section in soup.find_all("section", class_="marketsList", limit=1):
-    for index, li in enumerate(section.find_all("li")):
-        if index == 0:
-            continue
-
-        name_span = li.find("span", class_="name")
-        value_span = li.find("span", class_="value")
-        percentage_span = li.find("span", class_="percentage")
-
-        name = name_span.get_text().strip().lower().title() if name_span else "N/A"
-        value = value_span.get_text().strip() if value_span else "N/A"
-        percentage = (
-            list(percentage_span.stripped_strings)[-1] if percentage_span else "N/A"
-        )
-
-        price = f"\033[1m{name}\033[0m -> {value}, Variación: {percentage}"
-        prices.append(price)
+for div in soup.find_all(class_="exchange-dolar-container"):
+    for item in div:
+        name = item.find(class_="exchange-dolar-title").get_text()
+        amount = item.find(class_="exchange-dolar-amount").get_text()
+        variation = item.find(class_="exchange-dolar-percentage").get_text()
+        prices.append(f"{name}: {amount} ({variation})")
 
 
-# Function to format the tweet content
-def format_tweet_content(prices):
-    formatted_prices = []
-    for price in prices:
-        # Split the string and reformat
-        name, values = price.split(" -> ")
-        value, variation = values.split(", Variación: ")
-        formatted_price = f"{name}: {value} ({variation})"
-        formatted_prices.append(formatted_price)
+for tr in soup2.find_all("tr"):
+    name = tr.find("b")
+    value = tr.find("span", class_="ml15")
+    if name and value:
+        print("--------------------------")
+        print(f"{name.text.strip()}: {value.text.strip()}")
 
-    return "\n".join(formatted_prices)
+# Timezone object for Buenos Aires
+buenos_aires = pytz.timezone('America/Argentina/Buenos_Aires')
+buenos_aires_time = datetime.now(buenos_aires)
+# Extract the hour and minutes
+current_hour = buenos_aires_time.hour
+current_minute = buenos_aires_time.minute
 
-
-# Format the tweet
-tweet_content = format_tweet_content(prices)
-print(tweet_content)
-# # Check if tweet content needs to be split
-
-try:
-    tweet_length_limit = 280
-    if len(tweet_content) <= tweet_length_limit:
-        twitter.create_tweet(tweet_content)
-    else:
-        print("Tweet content surpasses the max length")
-except Exception as e:
-    print(f"An error occurred: {e}")
+tweet = f"Mercados a las {current_hour}:{
+    current_minute:02d}:\n\n{"\n".join(prices)}"
+# twitter.create_tweet(tweet)
